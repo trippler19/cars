d1df209c65ff04e248dd847063a7b79781cacf3b
Nikki Trippler <trippler@chapman.edu>
2018-11-30 11:48:58 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# cars.sh
+#Nikki Trippler
+
+echo "My Old Cars Inventory System"
+read My_Old_Cars
+echo "1. Enter a Car"
+echo "2. List the Cars"
+echo "Q. Quit"
+while echo $My_Old_Cars |
+do 
+	echo "1"
+	echo "Tell me about the car."
+	echo "What is the year?"
+	case "
