544f1772563a8610d960f28aa28e0d804bdf48c0
root <root@DESKTOP-KL3733C.localdomain>
2018-12-04 15:17:02 -0800

    Adds cars.sh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cars
+# Nikki Trippler
+# README file for cars.sh
+# This script asks the user to enter a car or sort the previously entered cars by year.
+
--- cars.sh
+++ cars.sh
@@ -3,13 +3,32 @@
 #Nikki Trippler
 
 echo "My Old Cars Inventory System"
-read My_Old_Cars
-echo "1. Enter a Car"
-echo "2. List the Cars"
-echo "Q. Quit"
-while echo $My_Old_Cars |
+stay=true
+while $stay
 do 
-	echo "1"
-	echo "Tell me about the car."
-	echo "What is the year?"
-	case "
+	echo "1. Enter a Car"
+	echo "2. List the Cars"
+	echo "q. Quit"
+	read chooseOne
+	case $chooseOne in
+	"1")
+		echo "Tell me about the car."
+		echo "What is the year?"
+		read carYear
+		echo "Who makes the car?"
+		read carMake
+		echo "What is the model of the car?"
+		read carModel
+		carName=$carYear":"$carMake":"$carModel
+		echo $carName >> "My_old_cars.txt"
+		;;
+	"2")
+		sort -n "My_old_cars.txt"
+		;;
+	"q")
+		stay=false
+		;;
+	esac
+done
+echo "Goodbye!"
+
